aa643fe565002dd4ba9c8d561f3a85ab7349a540
Ahmed Ch <ahmedchouihi@gmail.com>
2021-09-19 11:29:36 +0100

    'initialCommit'

---
+++ resources/views/test.blade.php
@@ -0,0 +1,13 @@
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+    <input type="hidden" name="business"
+        value="noshenhussain@gmail.com">
+    <input type="hidden" name="cmd" value="_donations">
+    <input type="hidden" name="item_name" value="Friends of the Park">
+    <input type="hidden" name="item_number" value="Fall Cleanup Campaign">
+    <input type="hidden" name="currency_code" value="USD">
+    <input type="image" name="submit" src="https://www.paypalobjects.com/webstatic/en_US/i/btn/png/btn_donate_92x26.png"
+    alt="Donate">
+    <img alt="" width="1" height="1"
+    src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" >
+
+</form>
